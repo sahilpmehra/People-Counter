8b202af4ffa06e69b3cc5e6a3c1de0f058c5ec6d
Sahil Mehra <sahilpmehra@yahoo.co.in>
2018-06-19 16:01:11 +0530

    Debugging

--- PeopleCounterMain.py
+++ PeopleCounterMain.py
@@ -62,7 +62,7 @@ while True:
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
-    _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # loop over the contours
     for c in cnts:
         # if the contour is too small, ignore it
